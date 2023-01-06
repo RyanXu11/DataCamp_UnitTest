377ed93ce8c67700f70cbf1756360b9deda6386c
Ryan.Xu11 <ryan.y.xu@hotmail.com>
2023-01-05 22:03:25 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup 
+setup(name='DataCamp_UnitTest', 
+      version='0.0.1', 
+      description='An example Unit Test from DataCamp Course', 
+      author='Ryan Xu', 
+      author_email='ptcloudtech@gmail.com', 
+      packages=['DataCamp_UnitTest'], 
+      install_requires=['matplotlib', 
+                        'numpy>=1.15.4', 
+                        'pycodestyle>=2.4.0'])
